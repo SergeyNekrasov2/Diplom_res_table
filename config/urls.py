d372b4a1d3453d5126095d98e393e75b6a55f404
Sergey <nekrasov-nekras-nekrasov@mail.ru>
2025-09-14 23:27:51 +0300

    .

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("reservation.urls", namespace="main")),
     path("users/", include("users.urls", namespace="users")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
